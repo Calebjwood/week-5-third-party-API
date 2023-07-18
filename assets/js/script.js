126ce61b0e6f173a5141e8e8e8147804d46213b1
unknown <Calebjwood60@gmail.com>
2023-07-18 04:39:44 -0500

    add the color cordnation

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,21 +8,122 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  var today = dayjs()
+  
+
+
+  
+  var today = dayjs() 
   $("#currentDay").text(today.format("dddd, MMMM D, YYYY"))
 
+  
+  var hourNineAm = $('#hour-9')
+  var hourTenAm = $("#hour-10")
+  var hourElevenAm = $("#hour-11")
+  var hourTwelvePm = $('#hour-12')
+  var hourOnePm = $('#hour-13')
+  var hourTwoPm = $('#hour-14')
+  var hourThreePm = $('#hour-15')
+  var hourFourPm = $('#hour-16')
+  var hourFivePm = $('#hour-17')
+  
+
+  
+
+  if(parseInt(hourNineAm[0].innerText[0])  > today.$H){
+  hourNineAm.addClass("future")
+    }else if(
+      parseInt(hourNineAm[0].innerText[0]) < today.$H){
+      hourNineAm.addClass("past")
+      }else{
+      hourNineAm.addClass("present")
+      }
+
+  
+  if(parseInt(hourTenAm[0].innerText[0])*10 + parseInt(hourTenAm[0].innerText[1])  > today.$H){
+  hourTenAm.addClass("future")
+    }else if(
+      parseInt(hourTenAm[0].innerText[0])*10 + parseInt(hourTenAm[0].innerText[1]) < today.$H){
+      hourTenAm.addClass("past")
+      }else{
+      hourTenAm.addClass("present")
+      }
 
 
+  if(parseInt(hourElevenAm[0].innerText[0])*10 +  parseInt(hourElevenAm[0].innerText[1])  > today.$H){
+  hourElevenAm.addClass("future")
+    }else if(
+      parseInt(hourElevenAm[0].innerText[0])*10 +  parseInt(hourElevenAm[0].innerText[1]) < today.$H){
+      hourElevenAm.addClass("past")
+      }else{
+      hourElevenAm.addClass("present")
+      }
 
+
+  if(parseInt(hourTwelvePm[0].innerText[0])*10 +  parseInt(hourTwelvePm[0].innerText[1]) > today.$H){
+  hourTwelvePm.addClass("future")
+    }else if(
+      parseInt(hourTwelvePm[0].innerText[0])*10 +  parseInt(hourTwelvePm[0].innerText[1]) < today.$H){
+      hourTwelvePm.addClass("past")
+      }else{
+      hourTwelvePm.addClass("present")
+      }
+  
+
+  if(parseInt(hourOnePm[0].innerText[0]) + 12 > today.$H){
+    hourOnePm.addClass("future")
+    }else if(
+      parseInt(hourOnePm[0].innerText[0]) + 12 < today.$H){
+      hourOnePm.addClass("past")
+      }else{
+      hourOnePm.addClass("present")
+      }
+
+
+  if(parseInt(hourTwoPm[0].innerText[0]) + 12 > today.$H){
+    hourTwoPm.addClass("future")
+    }else if(
+      parseInt(hourTwoPm[0].innerText[0]) + 12 < today.$H){
+      hourTwoPm.addClass("past")
+      }else{
+      hourTwoPm.addClass("present")
+      }
+
+      
+  if(parseInt(hourThreePm[0].innerText[0]) + 12 > today.$H){
+    hourThreePm.addClass("future")
+    }else if(
+      parseInt(hourThreePm[0].innerText[0]) + 12 < today.$H){
+      hourThreePm.addClass("past")
+      }else{
+      hourThreePm.addClass("present")
+      }
+  
+
+  if(parseInt(hourFourPm[0].innerText[0]) + 12 > today.$H){
+    hourFourPm.addClass("future")
+    }else if(
+      parseInt(hourFourPm[0].innerText[0]) + 12 < today.$H){
+      hourFourPm.addClass("past")
+      }else{
+      hourFourPm.addClass("present")
+      }
+
+      
+  if(parseInt(hourFivePm[0].innerText[0]) + 12 > today.$H){
+    hourFivePm.addClass("future")
+    }else if(
+      parseInt(hourFivePm[0].innerText[0]) + 12 < today.$H){
+      hourFivePm.addClass("past")
+      }else{
+      hourFivePm.addClass("present")
+      }
+
+  
+
+  
 });
